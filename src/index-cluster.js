ea394a2948193b2da2429f04a998ad909da78bd6
Manh Pham <manhpham90vn@icloud.com>
2024-12-07 12:19:52 +0700

    update

--- src/index-cluster.js
+++ src/index-cluster.js
@@ -1,31 +1,36 @@
 const Redis = require("ioredis");
 
-const cluster = new Redis.Cluster([
+const cluster = new Redis.Cluster(
+  [
+    {
+      host: "localhost",
+      port: 6379,
+    },
+    {
+      host: "localhost",
+      port: 6380,
+    },
+    {
+      host: "localhost",
+      port: 6381,
+    },
+    {
+      host: "localhost",
+      port: 6382,
+    },
+    {
+      host: "localhost",
+      port: 6383,
+    },
+    {
+      host: "localhost",
+      port: 6384,
+    },
+  ],
   {
-    host: "localhost",
-    port: 6379,
-  },
-  {
-    host: "localhost",
-    port: 6380,
-  },
-  {
-    host: "localhost",
-    port: 6381,
-  },
-  {
-    host: "localhost",
-    port: 6382,
-  },
-  {
-    host: "localhost",
-    port: 6383,
-  },
-  {
-    host: "localhost",
-    port: 6384,
-  },
-]);
+    scaleReads: "slave",
+  }
+);
 
 cluster.on("error", (err) => {
   console.error("Cluster error: ", err);
@@ -43,11 +48,8 @@ cluster.on("node error", (err, node) => {
   console.error(`Error on node ${node.options.host}:${node.options.port}`, err);
 });
 
-
 const testPrimaryWrite = async () => {
   try {
-    console.log("cluster: ", cluster);
-
     const ping = await cluster.ping();
     console.log("response ping: ", ping);
 
@@ -59,6 +61,6 @@ const testPrimaryWrite = async () => {
   } catch (error) {
     console.error("Error occurred: ", error);
   }
-}
+};
 
-testPrimaryWrite()
+testPrimaryWrite();
